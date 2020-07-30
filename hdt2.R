1d7365416052392a0d02554dab23e64d2fd977a0
Gustavo de Leon <gdeleontobias@gmail.com>
2020-07-30 17:07:43 -0600

    hola RODRIGO ZEA

--- hdt2.R
+++ hdt2.R
@@ -17,4 +17,8 @@ nombresVar<-datosImp[1,]
 datosImp<-as.data.frame(datosImp[2:nrow(datosImp),])
 nombresVar[c(1,4,5,6,8,10,11,15,16,21,23,24)]<-c("Anio","GasAviacion","GasSuperior","GasRegular","rTurboJet","DieselLS","DieselULS","AceitesLub","GrasasLub","PetroleoReconst","Orimulsion","MezclasOleosas")
 names(datosImp)<-nombresVar
+View(datosImp)
+
+datosImp <- datosImp[-c(46, 96, 146, 196), ]
+
 
