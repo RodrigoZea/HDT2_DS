ee529e24c19d01b117f114f3d56dd843dbf94bad
Rodrigo Zea <zearodrigo37@gmail.com>
2020-07-30 16:46:34 -0600

    Commit inicial, subiendo lectura de PDF en R

---
+++ hdt2.R
@@ -0,0 +1,20 @@
+# HDT #2 - Series de Tiempo
+# Rodrigo Zea - 17058
+# Gustavo de Leon - 17085
+# Sebastian Arriola - 11463
+
+library(forecast)
+library(tseries)
+library(fUnitRoots)
+library(ggfortify)
+library(tabulizer)
+library(dplyr)
+library(stringr)
+
+pages<-extract_tables("data.pdf") #datos2020
+datosImp <- do.call(rbind, pages)
+nombresVar<-datosImp[1,]
+datosImp<-as.data.frame(datosImp[2:nrow(datosImp),])
+nombresVar[c(1,4,5,6,8,10,11,15,16,21,23,24)]<-c("Anio","GasAviacion","GasSuperior","GasRegular","rTurboJet","DieselLS","DieselULS","AceitesLub","GrasasLub","PetroleoReconst","Orimulsion","MezclasOleosas")
+names(datosImp)<-nombresVar
+
